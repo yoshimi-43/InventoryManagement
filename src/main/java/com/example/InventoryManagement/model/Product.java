483b92da243ebe1d49150ad80147ae91c454cbbb
yoshi <irieyoshimi43+1@gmail.com>
2025-10-24 09:48:48 +0900

    コードリファクタリング

--- src/main/java/com/example/InventoryManagement/model/Product.java
+++ src/main/java/com/example/InventoryManagement/model/Product.java
@@ -11,7 +11,16 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "products")
 public class Product {
     @Id
@@ -32,49 +41,9 @@ public class Product {
     @Column(name = "unit_price", nullable = false)
     private Integer unitPrice;
 
-    public Product() {
-    }
-
-    public Product(String name, Integer quantity, Integer unitPrice) {
-        this.name = name;
-        this.quantity = quantity;
-        this.unitPrice = unitPrice;
-    }
-
-    // getters / setters
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) { this.id = id; }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-	public Integer getQuantity() {
-		return quantity;
-	}
-
-	public void setQuantity(Integer quantity) {
-		this.quantity = quantity;
-	}
-
-	public Integer getUnitPrice() {
-		return unitPrice;
-	}
-
-	public void setUnitPrice(Integer unitPrice) {
-		this.unitPrice = unitPrice;
-	}
-
     @Transient
     public Integer getTotalPrice() {
-    	// NullPointerExceptionを回避するためにnullチェックを強化
+        // NullPointerExceptionを回避するためにnullチェックを強化
         if (quantity == null || unitPrice == null) {
             return 0;
         }
